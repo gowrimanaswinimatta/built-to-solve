a7e5d8a3da65871c2d30f07c1c92a1fa237c4de4
gowrimanaswinimatta <gowrimanaswinimatta@gmail.com>
2026-07-22 15:21:10 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Header from "@/components/layout/Header";
+import Footer from "@/components/layout/Footer";
 
 export const metadata: Metadata = {
   title: "BUILT TO SOLVE.",
