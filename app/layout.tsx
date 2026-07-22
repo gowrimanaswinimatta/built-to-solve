04708f54a47bba815e5fc372eb9c67dbaa5bf87b
gowrimanaswinimatta <gowrimanaswinimatta@gmail.com>
2026-07-22 15:14:11 -0500

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,20 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "BUILT TO SOLVE.",
+  description:
+    "Building products that transform complex ideas into measurable impact."
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
